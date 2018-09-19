e53e764256161b803a7ffd3e07c3b997179a02ad
Satish.Marathe <marathesinpune@gmail.com>
2018-09-19 14:36:59 +0530

    initial commit

---
+++ src/main/java/com/filedownload/SpringBootJerseyFileDownloadExApplication.java
@@ -0,0 +1,12 @@
+package com.filedownload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJerseyFileDownloadExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJerseyFileDownloadExApplication.class, args);
+	}
+}
---
+++ src/test/java/com/filedownload/SpringBootJerseyFileDownloadExApplicationTests.java
@@ -0,0 +1,16 @@
+package com.filedownload;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootJerseyFileDownloadExApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
