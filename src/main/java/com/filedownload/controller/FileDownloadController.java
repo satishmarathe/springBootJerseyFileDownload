6ea6c43314845a81dde9e985e862ae8a9daf48ed
Satish.Marathe <marathesinpune@gmail.com>
2018-09-19 17:08:03 +0530

    added a rest end point method for pdf file download

--- src/main/java/com/filedownload/controller/FileDownloadController.java
+++ src/main/java/com/filedownload/controller/FileDownloadController.java
@@ -1,11 +1,18 @@
 package com.filedownload.controller;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
 
 import org.springframework.stereotype.Component;
 
 @Component
 @Path("/download")
 public class FileDownloadController {
-
+	
+	@GET
+    @Path("/pdf")
+    public Response downloadPdfFile(){
+		return null;
+	}
 }
