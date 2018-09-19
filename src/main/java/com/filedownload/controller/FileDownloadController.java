a3ca64f159291a464c0f857aab85a58b09dd8d63
Satish.Marathe <marathesinpune@gmail.com>
2018-09-19 17:04:27 +0530

    Added new controller for FileDownload

---
+++ src/main/java/com/filedownload/controller/FileDownloadController.java
@@ -0,0 +1,11 @@
+package com.filedownload.controller;
+
+import javax.ws.rs.Path;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("/download")
+public class FileDownloadController {
+
+}
